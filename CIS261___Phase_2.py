d82583a28d796bd4e69ae861b0540d66b4bed172
strayer_practice@outlook.com <strayer_practice@outlook.com>
2023-02-19 11:14:29 -0800

    Update

--- CIS261___Phase_2.py
+++ CIS261___Phase_2.py
@@ -39,10 +39,11 @@ def printinfo(EmpDetailList):
     # the following code creates a for loop to read through EmpDetailList and assign values in list to variables
     for EmpList in EmpDetailList:
         fromdate = EmpList[0]
-        #write code to assign values to todate, empname, hours, hourlyrate, and taxrate from EmpLst
-
-
-
+        todate = EmpList[1]
+        empname = EmpList[2]
+        hours = EmpList[3]
+        hourlyrate = EmpList[4]
+        taxrate = EmpList[5]
 
         grosspay, incometax, netpay = CalcTaxAndNetPay(hours, hourlyrate, taxrate)
         print(fromdate, todate, empname, f"{hours:,.2f}",  f"{hourlyrate:,.2f}", f"{grosspay:,.2f}",  f"{taxrate:,.1%}",  f"{incometax:,.2f}",  f"{netpay:,.2f}")
@@ -54,7 +55,7 @@ def printinfo(EmpDetailList):
         # the following line of code assigns TotEmployees totals to dictionary 
         EmpTotals["TotEmp"] = TotEmployees
         # write code to assign TotHours, TotGrossPay, TotTax, and TotNetPay to corresponding dictionary item
-
+        EmpTotals["TotHours"] = 
 
 
 
